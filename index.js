6dad63123c7ebaf34e0d90f67656c4a4f00514af
AbhinabK <abhinabk123@gmail.com>
2023-03-13 19:49:01 +0530

    single id page working

--- index.js
+++ index.js
@@ -11,20 +11,24 @@ app.set('views', path.join(__dirname, '/views'))
 app.use(express.urlencoded({ extended: true }))
 
 const comments = [
-    {
+    {   
+        id: 1,
         username: 'Todd',
         comment: 'lol that is so funny'
 
     },
     {
+        id:2,
         username: 'Wodd',
         comment: 'no that is no so funny'
 
     }, {
+        id:2,
         username: 'Modd',
         comment: 'that might be funny'
 
     }, {
+        id:4,
         username: 'Lodd',
         comment: 'not funny'
 
@@ -43,6 +47,12 @@ app.post('/comments',(req,res)=>{
 app.get('/comments/new',(req,res)=>{
     // renders a form to add a new comment which will be send using a POST request
     res.render('comments/new.ejs')
+})
+app.get('/comments/:id',(req,res)=>{
+    const {id} = req.params;
+    // find the relevent comment form id
+    const comment  = comments.find(c => c.id === parseInt(id))
+    res.render('comments/show.ejs',{comment})
 })
 app.listen(3000, () => {
     console.log("Listning on 3000");
